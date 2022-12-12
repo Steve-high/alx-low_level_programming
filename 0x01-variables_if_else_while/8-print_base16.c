f1c9ffd3a924b088bb2b51997f35d422b8245b4c
Steve-high <stephenmicheal43gmail.com>
2022-12-12 14:13:32 -0800

    8digit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ * followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	for (b = 97; b <= 102; b++)
+		putchar(b);
+	putchar('\n');
+	return (0)
+}
