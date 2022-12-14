323f578da9d08d748974aa19641d6f2f3a023dd9
Steve-high <stephenmicheal43gmail.com>
2022-12-14 10:49:33 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - program that prints
+ * _putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *wh = "_putchar";
+
+	while (*wh)
+	{
+		_putchar(*wh);
+		wh++;
+	}
+	_putchar('\n');
+	return (0)
+}
